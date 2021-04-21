757c1b37bd76e3a059a2eddb1341ce01c8775eab
103620582 <103620582@student.swin.edu.au>
2021-04-21 17:22:26 +1000

    Exercise1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int num = 5;
+        int counter = 0;
+        int counter2 = 0;
+
+            while (counter < num) {
+                counter2 = counter;
+
+                while (counter2 < num) {
+                    Console.Write("x");
+                    counter2++;
+                }
+                Console.WriteLine();
+                counter++;
+            }
+
+
+        }
+    }
+}
