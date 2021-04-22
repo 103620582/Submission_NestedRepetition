73b93fe337e668de9ac0c5130263937a5685ba2d
103620582 <80442207+103620582@users.noreply.github.com>
2021-04-22 13:29:54 +1000

    Add files via upload

---
+++ Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of x's.");
+            string number = Console.ReadLine();
+
+            int num1 = int.Parse(number);
+
+            int counter = 0;
+            int counter2 = 0;
+
+            while (counter < num1) {
+                counter2 = counter;
+
+                while (counter2 < num1) {
+                    Console.Write("x");
+                    counter2++;
+                }
+
+                Console.WriteLine();
+                counter++;
+            }
+
+            Console.WriteLine("Thanks, cya!");
+
+
+        }
+    }
+}
