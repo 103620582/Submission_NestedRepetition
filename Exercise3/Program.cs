ea0e4e88db77e72d4cea5c099ff5acd4547264d1
103620582 <80442207+103620582@users.noreply.github.com>
2021-04-23 19:40:54 +1000

    Add files via upload

---
+++ Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int num = 5;
+        int counter1 = 0;
+        
+        while (counter1 < num) {
+        int counter2 = 0;
+            while (counter2++ < counter1)
+            Console.Write("x");
+            Console.WriteLine();
+            counter1++;
+        
+        } 
+
+
+        }
+    }
+}
