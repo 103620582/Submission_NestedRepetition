40cc2d19445854cd913d0e1990d4146eca3b107b
103620582 <80442207+103620582@users.noreply.github.com>
2021-04-23 19:49:53 +1000

    Add files via upload

---
+++ Exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        Console.WriteLine("Please enter how many x's");
+        string num1 = Console.ReadLine();
+    
+        int num = int.Parse(num1);
+        int counter1 = 0;
+        
+        while (counter1 < num) {
+        int counter2 = 0;
+
+            while (counter2++ < counter1)
+            Console.Write("x");
+            Console.WriteLine();
+            counter1++;
+        
+        } 
+
+        Console.WriteLine("Cheerz, cya!");
+
+        }
+    }
+}
